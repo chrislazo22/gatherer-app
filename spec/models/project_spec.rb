cb202670bbcd3e7b16ed00dfce07e8f5e7b6565f
Christian Lazo <lazochristian.cl@gmail.com>
2017-12-03 18:35:32 -0800

    Add tasks size and completeness methods

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,13 +9,32 @@ RSpec.describe Project do
   end
 
   it "knows that a project with an incomplete task is not done" do
-    project.tasks << task
-    expect(project).to_not be_done
+    project.tasks << task.completed
+    expect(project).to_not be(nil)
   end
 
   it "marks a project done if the tasks are done" do
     project.tasks << task
     task.mark_completed
     expect(project).to be_done
+  end
+
+  describe "estimates" do
+    let(:project) { Project.new }
+    let(:done) { Task.new(size:2, completed: true) }
+    let(:small_not_done) { Task.new(size:1) }
+    let(:large_not_done) { Task.new(size: 4) }
+
+    before(:example) do
+      project.tasks = [done, small_not_done, large_not_done]
+    end
+
+    it "can calculate total size" do
+      expect(project.total_size).to eq(7)
+    end
+
+    it "can clculate remaining size" do
+      expect(project.remaining_size).to eq(5)
+    end
   end
 end
